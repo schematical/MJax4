cbb81ccf24f54607d18ff3066cdfe69d113a5ed0
Matt Lea <mlea@schematical.com>
2013-09-28 16:12:29 -0500

    Again PHPStorm is a bastard

--- _core/assets/js/MJax.js
+++ _core/assets/js/MJax.js
@@ -1,5 +1,25 @@
 (function(){
     window.MJax = MJax = {
+        Actions:{
+            'MJaxServerControlAction':function(objEvent){
+                var objControl = $(this).data('mjax-control');
+                //Figure out a priority thing
+                var objData = {};
+                objData.action = 'control_event';
+                objData.control_id = objControl.ControlId;
+                objData.event = objEvent.type;
+                //objData.MJaxForm__FormState = strFormState;
+                if(objEvent.type == 'keypress'){
+                    objData.keyCode = objEvent.keyCode;
+                }
+                for(var strControlId in MJax.Controls){
+                    objData[strControlId] = MJax.Serialize(MJax.Controls[strControlId]);
+                }
+
+                MJax.Connections['ajax'].TriggerEvent(objData);
+            }
+        },
+        Connections:{},
         ControlDefinitions:{},
         Controls:{},
         Init:function(objFormData){
@@ -24,10 +44,10 @@
                 MJax.FormData.head.require
             );
             requirejs(
-                ['jquery','jquery_mobile', 'mustache'],//, 'require_text'],
+                ['jquery','jquery_mobile', 'mustache','MJaxAjaxConn'],//, 'require_text'],
                 function($, jMobile, Mustache) {
                     window.Mustache = Mustache;
-
+                    MJax.Connections['ajax'] = new MJaxAjaxConn();
                     MJax.InitControlDefinitions();
                 }
             );
@@ -48,6 +68,9 @@
 
             }
             MJax.Render();
+            for(var strId in MJax.Controls){
+                MJax.Controls[strId].Attach();
+            }
         },
         Render:function(){
             MJax.LoadTemplate();
@@ -58,6 +81,50 @@
             $('body').html(
                 strBody
             );
+        },
+        Update:function(objJson){
+
+        },
+        Error:function(objJson){
+
+        },
+        Serialize:function(mixData){
+            if(mixData == null){
+                return null;
+            }
+            var strJType = typeof(mixData);
+            if(strJType == 'object'){
+                if(typeof(mixData['MSerialize']) != 'undefined'){
+                    return mixData.MSerialize();
+                }
+                var objReturn = {};
+                for(var strKey in mixData){
+                    var mixChildData  = MJax.Serialize(mixData[strKey]);
+                    if(typeof(mixChildData) != 'undefined'){
+                        objReturn[strKey] = mixChildData;
+                    }
+                }
+            }else if(strJType == 'array'){
+
+                var objReturn = [];
+                for(var strKey in mixData){
+                    var mixChildData  = MJax.Serialize(mixData[strKey]);
+                    if(typeof(mixChildData) != 'undefined'){
+                        objReturn[strKey] = mixChildData;
+                    }
+                }
+            }else if(
+                //Put types to ignore here
+                (strJType == 'function')
+            ){
+                //Do nothing
+            }else{
+                var objReturn = mixData;
+            }
+            return objReturn;
+        },
+        TriggerEvent:function(objEvent){
+
         }
 
     };
